107751370cdff7e96406bbd76f9e1f1f7c536308
rambabu.ramaraj <rambabu.ramaraj@siamcomputing.com>
2021-11-17 13:17:20 +0530

    rename dist file

--- src/app/server.js
+++ src/app/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static('./dist/task'));
-app.get('/*',(req,res)=> res.sendFile('index.html',{ShadowRoot:'dist/task'}));
+app.use(express.static('./dist/arr'));
+app.get('/*',(req,res)=> res.sendFile('index.html',{ShadowRoot:'dist/arr'}));
     app.listen(process.env.PORT || 8000);
 
